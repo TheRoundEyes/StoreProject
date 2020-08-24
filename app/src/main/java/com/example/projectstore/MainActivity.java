12d02fb2c519e55512c1c60fb7c9768eea94ebbf
TheRoundEyes <theroundeyess@gmail.com>
2020-08-24 22:56:36 +1200

    Push 24/08/2020 - 10:56pm

--- app/src/main/java/com/example/projectstore/MainActivity.java
+++ app/src/main/java/com/example/projectstore/MainActivity.java
@@ -69,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(i);
+                System.out.println("Marvin");
+                startActivity(i);   
             }
         });
     }
